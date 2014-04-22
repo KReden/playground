d346d1b7cd7ac5cd7efad6ccef9e874ed7e41587
Kyle Reden <kreden@urbanbound.com>
2014-04-22 18:23:25 -0500

    test commit

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -2,7 +2,7 @@
  * Created by kylereden on 4/2/14.
  */
 
-var exec            = require('child_process').exec
+var exec          = require('child_process').exec
   , queryString   = require('querystring')
   , fs            = require('fs')
   , formidable    = require('formidable');
@@ -10,35 +10,17 @@ var exec            = require('child_process').exec
 function start(response){
   console.log('start request');
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" enctype="multipart/form-data" method="post">'+
-    '<input type="file" name="upload" multiple="multiple">'+
-    '<input type="submit" value="Upload file" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
-
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
   response.end();
 
-
-//    exec('ls -lah', function(error, stdout, stderr){
-//        response.writeHead(200, {"Content-Type": "text/plain"});
-//        response.write(stdout);
-//        response.end();
-//    });
 }
 
 function show(response){
   console.log('show request');
   response.writeHead(200, {"Content-Type": "img/png"});
-  fs.createReadStream('app/assets/img/test.png').pipe(response);
+//  fs.createReadStream('app/assets/img/test.png').pipe(response);
+  response.write('../app/views/index');
 }
 
 function upload(response, request){
@@ -61,7 +43,7 @@ function upload(response, request){
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write('recieved image <br/>');
-    response.write("<img src='/show' />");
+//    response.write("<img src='/show' />");
     response.end();
   });
 }
